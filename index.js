7a2199e09ad558d73fe856cbfdcb6870162f277f
shahnawaz-alam37 <89523564+shahnawaz-alam37@users.noreply.github.com>
2023-09-21 15:13:50 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 // run `node index.js` in the terminal
+import express from "express";
+import qr from "qr-image";
+import bodyparser from "body-parser";
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+const app = express();
+
+app.use(bodyparser.urlencoded({ extended: false }))
+
+app.get("/", (req, res) =>{
+  res.render("index.ejs");
+});
+
+app.post("/generate", (req, res) =>{
+  var link = req.body.link;
+  console.log(link);
+});
+
+app.listen(3000, ()=>{
+  console.log("running on port 3000")
+});
